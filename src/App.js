3e1e4c3954b3ee5c1abf8da7fe1d1587f498d84c
AlexhHr23 <alex.hernandez24kb@gmail.com>
2026-04-23 17:07:30 -0600

    styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Container, Row, Col} from "react-bootstrap"
 import { useEffect, useState } from "react";
 import axios from "axios";
+import "./App.css"
 
 function App() {
 
@@ -10,11 +11,11 @@ function App() {
     const  fetchData = async () =>  {
 
       try {
-        const apiUrl = "http://localhost:8080";
+        const apiUrl = process.env.REACT_APP_API_ROOT;
         const response = await axios.get(apiUrl);
 
-        if (response.status === 200) {
-          setapiData(response.data);
+        if (response?.data?.statusText === "Ok") {
+          setapiData(response?.data?.blog_records);
         }
       } catch (error) {
         console.log(error);
@@ -33,11 +34,20 @@ function App() {
   return (
     <Container>
       <Row>
-        <Col xs="12 py-2">
+        <Col xs="12" className="py-2">
         <h1 className="text-center fw-bold">
           React application wuth go fiber backend
         </h1>
         </Col>
+
+        {apiData && (
+          apiData.map((record, index) => (
+            <Col xs="4" className="py-5 box" key={index}>
+              <div className="title">{record.title}</div>
+              <div>{record.post}</div>
+            </Col>
+          ))
+        )}
       </Row>
     </Container>
   );
