c3a8a9e068e3b59c580217081390587dd5170783
AlexhHr23 <alex.hernandez24kb@gmail.com>
2026-04-24 17:06:56 -0600

    page delete

---
+++ src/page/Delete.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+export const Delete = () => {
+  return (
+   <>
+   <Container>
+    <h1></h1>
+   </Container>
+   </>
+  )
+}
